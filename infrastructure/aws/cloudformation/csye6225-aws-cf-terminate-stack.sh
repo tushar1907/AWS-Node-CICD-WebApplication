fee9c5e8980eadbeda240be86698157f485aa0b0
tushar1907 <gupta.tus@husky.neu.edu>
2018-09-27 14:40:19 -0400

    terminate stack cloud formation done

--- infrastructure/aws/cloudformation/csye6225-aws-cf-terminate-stack.sh
+++ infrastructure/aws/cloudformation/csye6225-aws-cf-terminate-stack.sh
@@ -1,3 +1,14 @@
 stackname=$1
-v=$(aws cloudformation delete-stack --stack-name $stackname)
-echo $v
+createOutput=$(aws cloudformation delete-stack --stack-name $stackname)
+
+if [ $? -eq 0 ]; then
+	echo "Deletion stack..."
+	aws cloudformation wait stack-delete-complete --stack-name $stackname
+	echo "Stack created successfully. Stack Id below: "
+
+	echo $createOutput
+
+else
+	echo "Error in creation of stack"
+	echo $createOutput
+fi;
