6a323573386afac1ec2fe59b7c49bd4c0f7d5f82
harsharma91 <32245540+harsharma91@users.noreply.github.com>
2018-09-27 04:13:18 -0400

    updated

--- infrastructure/aws/scripts/csye6225-aws-networking-teardown.sh
+++ infrastructure/aws/scripts/csye6225-aws-networking-teardown.sh
@@ -11,24 +11,4 @@ aws ec2 detach-internet-gateway --internet-gateway-id $IGW_ID --vpc-id $VPCID
 aws ec2 delete-internet-gateway --internet-gateway-id $IGW_ID
 aws ec2 delete-vpc --vpc-id $VPCID
 
-<<<<<<< HEAD
-# Delete route table 
-for i in `aws ec2 describe-route-tables --filters Name=vpc-id,Values="${vpcid}" | grep rtb- | sed -E 's/^.(rtb-[a-z0-9]+).$/\1/'`; do aws ec2 delete-route-table --route-table-id=$i; done
-
-# Delete route table 
-for i in `aws ec2 describe-route-tables --filters Name=vpc-id,Values="${vpcid}" | grep rtb- | sed -E 's/^.*(rtb-[a-z0-9]+).*$/\1/'`; do aws ec2 delete-route-table --route-table-id=$i; done
-
-# Delete subnets
-for i in `aws ec2 describe-subnets --filters Name=vpc-id,Values="${vpcid}" | grep subnet- | sed -E 's/^.*(subnet-[a-z0-9]+).*$/\1/'`; do aws ec2 delete-subnet --subnet-id=$i; done
-
-# Detach internet gateways
-for i in `aws ec2 describe-internet-gateways --filters Name=attachment.vpc-id,Values="${vpcid}" | grep igw- | sed -E 's/^.*(igw-[a-z0-9]+).*$/\1/'`; do aws ec2 detach-internet-gateway --internet-gateway-id=$i --vpc-id=$vpcid; done
-
-# Delete internet gateways
-for i in `aws ec2 describe-internet-gateways --filters Name=attachment.vpc-id,Values="${vpcid}" | grep igw- | sed -E 's/^.*(igw-[a-z0-9]+).*$/\1/'`; do aws ec2 delete-internet-gateway --internet-gateway-id=$i; done
-
-# Delete the VPC
-aws ec2 delete-vpc --vpc-id ${vpcid}
-=======
->>>>>>> 3c34ae17884aee17bc998e88c82d21856a9c6ba6
 rm ID.txt
