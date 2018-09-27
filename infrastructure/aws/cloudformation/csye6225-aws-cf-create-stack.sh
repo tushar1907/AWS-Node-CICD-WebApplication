cb5e22676c2d702b98468113263f3948cd94bad2
tushar1907 <gupta.tus@husky.neu.edu>
2018-09-27 14:17:53 -0400

    Error handing in cloud formation added

--- infrastructure/aws/cloudformation/csye6225-aws-cf-create-stack.sh
+++ infrastructure/aws/cloudformation/csye6225-aws-cf-create-stack.sh
@@ -1,4 +1,14 @@
 stackname=$1
-v=$(aws cloudformation create-stack --stack-name $stackname \
---template-body file://csye6225-cf-networking.json --parameters ParameterKey=stackname,ParameterValue=$stackname)
-echo $v
+createOutput=$(aws cloudformation create-stack --stack-name $stackname --template-body file://csye6225-cf-networking.json --parameters ParameterKey=stackname,ParameterValue=$stackname)
+
+if [ $? -eq 0 ]; then
+	echo "Creating stack..."
+	aws cloudformation wait stack-create-complete --stack-name $stackname
+	echo "Stack created successfully. Stack Id below: "
+
+	echo $createOutput
+
+else
+	echo "Error in creation of stack"
+	echo $createOutput
+fi;
