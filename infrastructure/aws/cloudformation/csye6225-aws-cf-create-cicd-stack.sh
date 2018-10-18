fe48183bdd7cd21c560899b36ea4adeee36dd891
Harsh Sharma <32245540+harsharma91@users.noreply.github.com>
2018-10-17 21:32:59 -0400

    cicd stack

--- infrastructure/aws/cloudformation/csye6225-aws-cf-create-cicd-stack.sh
+++ infrastructure/aws/cloudformation/csye6225-aws-cf-create-cicd-stack.sh
@@ -1,5 +1,19 @@
+#!/bin/bash
+#Variables
+
 stackname=$1
-createOutput=$(aws cloudformation create-stack --stack-name $stackname --template-body file://csye6225-cf-cicd.json --parameters ParameterKey=stackname,ParameterValue=$stackname)
+
+domain=$(aws route53 list-hosted-zones --query HostedZones[0].Name --output text)
+trimdomain=${domain::-1}
+s3domain="code-deploy.$trimdomain"
+echo "S3 Domain: $s3domain"
+accid=$(aws sts get-caller-identity --output text --query 'Account')
+echo "AccountId: $accid"
+
+
+createOutput=$(aws cloudformation create-stack --stack-name $stackname --capabilities CAPABILITY_NAMED_IAM --template-body file://csye6225-cf-ci-cd.json --parameters ParameterKey=s3domain,ParameterValue=$s3domain ParameterKey=accid,ParameterValue=$accid)
+
+
 if [ $? -eq 0 ]; then
 	echo "Creating stack..."
 	aws cloudformation wait stack-create-complete --stack-name $stackname
