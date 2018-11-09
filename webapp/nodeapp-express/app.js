52e04a7aa2034af8d512aea8f639809ed6b2984c
tushar1907 <33243998+tushar1907@users.noreply.github.com>
2018-11-09 00:47:33 -0500

    merge

--- webapp/nodeapp-express/app.js
+++ webapp/nodeapp-express/app.js
@@ -580,78 +580,26 @@ app.listen('3000',()=>{
 });
 
 //user password reset
-app.get('/user',(req,res)=>{
+app.get('/reset',(req,res)=>{
   var uuid = req.headers.uuid
-  let sql2="SELECT * from `user` where `uuid`='"+uuid+"'"
 
-      var AWS = require('aws-sdk');
-    
-
-    // Create publish parameters
+  var useremail = "gupt.tus@husky.neu.edu";
+  
+    var msg = useremail+"|"+process.env.EMAIL_SOURCE+"|"+process.env.DDB_TABLE+"|"+req.get('host');
+    console.log("Message is --> " + msg)
     var params = {
-      Message: 'MESSAGE_TEXT', /* required */
-      TopicArn: 'arn:aws:sns:us-east-1:801880070264:password_reset'
+      Message: msg, /* required */
+      TopicArn:process.env.TOPIC_ARN
     };
-
-    // Create promise and SNS service object
-    var publishTextPromise = new AWS.SNS({apiVersion: '2010-03-31'}).publish(params).promise();
-
-    // Handle promise's fulfilled/rejected states
-    publishTextPromise.then(
-      function(data) {
-        console.log(`Message ${params.Message} send sent to the topic ${params.TopicArn}`);
-        console.log("MessageID is " + data.MessageId);
-      }).catch(
-        function(err) {
-        console.error(err, err.stack);
-      });
-  // let query1=db.query(sql2,(err,result)=>{
-  //   if(err) throw err
-  //   if(result.length!==0){
-  //     //SES implementation// Create sendEmail params 
-  //     var emailParams = {
-  //       Destination: { /* required */
-  //         ToAddresses: [
-  //           'sharma.ha@husky.neu.edu'
-  //           //'anand.ak@husky.neu.edu'
-  //           /* more items */
-  //         ]
-  //       },
-  //       Message: { /* required */
-  //         Body: { /* required */
-  //           Html: {
-  //           Charset: "UTF-8",
-  //           Data: ""
-  //           },
-  //           Text: {
-  //           Charset: "UTF-8",
-  //           Data: "TEXT_FORMAT_BODY"
-  //           }
-  //         },
-  //         Subject: {
-  //           Charset: 'UTF-8',
-  //           Data: 'Mathworks Test Engineer Offer Letter:$40 per hour'
-  //         }
-  //         },
-  //       Source: 'gupta.tus@husky.neu.edu', /* required */
-  //       ReplyToAddresses: [
-  //           'gupta.tus@husky.neu.edu',
-  //         /* more items */
-  //       ],
-  //     }; 
-  //     // Create the promise and SES service object
-  //     var sendPromise = new AWS.SES({apiVersion: '2010-12-01'}).sendEmail(emailParams).promise();
-
-  //     sendPromise.then(
-  //       function(data) {
-  //         console.log(data.MessageId);
-  //       }).catch(
-  //         function(err) {
-  //         console.error(err, err.stack);
-  //       });
-  //         }
-  //       })
-  //       res.send({'msg':'Email send successfully'})
+    sns.publish(params, function(err, data) {
+      if (err) logger.info(err, err.stack); // an error occurred
+      else{
+        logger.info(data);
+        req.flash('success','Reset link sent successfully!');
+        res.redirect('/forgot');
+      }           // successful response
+    });
+  
 });
 
 
