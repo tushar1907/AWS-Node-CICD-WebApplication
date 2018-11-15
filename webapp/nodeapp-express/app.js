55d9524784b0e1982f438a0a6c5acf1ab738654e
tushar1907 <33243998+tushar1907@users.noreply.github.com>
2018-11-15 11:47:53 -0500

    nsu 5

--- webapp/nodeapp-express/app.js
+++ webapp/nodeapp-express/app.js
@@ -616,7 +616,7 @@ app.listen('3000',()=>{
 app.get('/reset',(req,res)=>{
   var uuid = req.headers.uuid
 
-  let sql1="select uuid from `user` where `uuid`='"+uuid+"'";
+  let sql1="select * from `user` where `uuid`='"+uuid+"'";
       let query1=db.query(sql1,(err,result)=>{
         
         logger.info(result.length);
