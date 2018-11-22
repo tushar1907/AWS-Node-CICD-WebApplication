e74712e36b257eaf6ac60017fad8f9e8846a67d0
Harsh Sharma <32245540+harsharma91@users.noreply.github.com>
2018-11-21 22:51:58 -0500

    stack updates assignment9

--- infrastructure/aws/cloudformation/csye6225-aws-cf-create-auto-scaling-application-stack.sh
+++ infrastructure/aws/cloudformation/csye6225-aws-cf-create-auto-scaling-application-stack.sh
@@ -11,8 +11,14 @@ vpc=$(aws ec2 describe-vpcs --filter "Name=tag:aws:cloudformation:stack-id,Value
 echo "VPC Id: $vpc"
 subnet1=$(aws ec2 describe-subnets --filter "Name=tag:Name,Values=$netstack-csye6225-ec2-pub-subnet1" --query Subnets[0].SubnetId --output text)
 echo "Subnet-1: $subnet1"
+subnet2=$(aws ec2 describe-subnets --filter "Name=tag:Name,Values=$netstack-csye6225-ec2-pub-subnet2" --query Subnets[0].SubnetId --output text)
+echo "Subnet-2: $subnet2"
+subnet3=$(aws ec2 describe-subnets --filter "Name=tag:Name,Values=$netstack-csye6225-ec2-pub-subnet3" --query Subnets[0].SubnetId --output text)
+echo "Subnet-3: $subnet3"
 dbsubnet=$(aws rds describe-db-subnet-groups  --query "DBSubnetGroups[?VpcId=='$vpc'].DBSubnetGroupName"  --output text)
 echo "DB Subnet Group Name: $dbsubnet"
+sglb=$(aws ec2 describe-security-groups --filters "Name=group-name,Values=$netstack-csye6225-lb-secuitygroup" --query SecurityGroups[*].GroupId --output text)
+echo "LB SG: $sglb"
 sgec2=$(aws ec2 describe-security-groups --filters "Name=group-name,Values=$netstack-csye6225-webapp-secuitygroup" --query SecurityGroups[*].GroupId --output text)
 echo "EC2 SG: $sgec2"
 sgdb=$(aws ec2 describe-security-groups --filters "Name=group-name,Values=$netstack-csye6225-db-secuitygroup" --query SecurityGroups[*].GroupId --output text)
@@ -25,7 +31,15 @@ s3domain="$trimdomain.csye6225.com"
 echo "S3 Bucket: $s3domain"
 snstopic=$(aws sns list-topics --query Topics[0] --output text)
 echo "SNS Topic arn is" $snstopic
-createOutput=$(aws cloudformation create-stack --stack-name $stackname --template-body file://csye6225-cf-application.json --parameters ParameterKey=stackname,ParameterValue=$stackname ParameterKey=dbsubnet,ParameterValue=$dbsubnet ParameterKey=s3domain,ParameterValue=$s3domain ParameterKey=ec2Subnet,ParameterValue=$subnet1 ParameterKey=ec2SecurityGroup,ParameterValue=$sgec2 ParameterKey=dbSecurityGroupId,ParameterValue=$sgdb ParameterKey=iaminstance,ParameterValue=$iaminstance ParameterKey=domainname,ParameterValue=$trimdomain ParameterKey=snsTopicArn,ParameterValue=$snstopic)
+SSLArn=$(aws acm list-certificates --query "CertificateSummaryList[?DomainName=='$trimdomain'].CertificateArn" --output text)
+echo "SSLArn: $SSLArn"
+appname="csye6225CodeDeployApplication"
+echo "appname: $appname"
+depname="csye6225CodeDeployApplication-depgroup"
+echo "depname: $depname"
+codeployRole=$(aws iam get-role --role-name CodeDeployServiceRole --query Role.Arn --output text)
+echo "CodeDeployServiceRole: $codeployRole"
+createOutput=$(aws cloudformation create-stack --stack-name $stackname --template-body file://csye6226-cf-auto-scaling-application.json --parameters ParameterKey=stackname,ParameterValue=$stackname ParameterKey=dbsubnet,ParameterValue=$dbsubnet ParameterKey=s3domain,ParameterValue=$s3domain ParameterKey=ec2Subnet1,ParameterValue=$subnet1 ParameterKey=ec2Subnet2,ParameterValue=$subnet2 ParameterKey=ec2Subnet3,ParameterValue=$subnet3 ParameterKey=ec2SecurityGroup,ParameterValue=$sgec2 ParameterKey=dbSecurityGroupId,ParameterValue=$sgdb ParameterKey=iaminstance,ParameterValue=$iaminstance ParameterKey=domainname,ParameterValue=$trimdomain ParameterKey=snsTopicArn,ParameterValue=$snstopic ParameterKey=sglb,ParameterValue=$sglb ParameterKey=SSLArn,ParameterValue=$SSLArn ParameterKey=vpc,ParameterValue=$vpc ParameterKey=appname,ParameterValue=$appname ParameterKey=depname,ParameterValue=$depname ParameterKey=codeployRole,ParameterValue=$codeployRole)
 
 if [ $? -eq 0 ]; then
 	echo "Creating stack..."
